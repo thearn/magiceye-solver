1e6c1b7546f707a6a95c7360ca023cdc270e0fc6
Tristan Hearn <tristanhearn@gmail.com>
2025-05-03 17:36:50 -0400

    fixed setup

--- setup.py
+++ setup.py
@@ -1,8 +1,17 @@
 from setuptools import setup, find_packages
 
+# Function to read requirements from requirements.txt
+def parse_requirements(filename):
+    """ load requirements from a pip requirements file """
+    lineiter = (line.strip() for line in open(filename))
+    return [line for line in lineiter if line and not line.startswith("#")]
+
+# Read requirements
+install_reqs = parse_requirements('requirements.txt')
+
 setup(name='magiceye_solve',
       version='0.1',
-      install_requires=['numpy', 'scipy', 'matplotlib'],
+      install_requires=install_reqs,
       description="Program that automatically solves magic eye autostereograms",
       author='Tristan Hearn',
       author_email='tristanhearn@gmail.com',
